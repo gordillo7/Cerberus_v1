b13363f052cc9cd0b4a5810757fd3e9d276e0646
gordillo7 <daviddgb782002@gmail.com>
2025-04-06 12:52:50 +0200

    Completar todolist.txt

--- modules/http_webscan.py
+++ modules/http_webscan.py
@@ -1,1 +1,3 @@
-"""Usar Nuclei"""
+"""Usar Nuclei con todos los subdominios (parametro -l, crear una lista temporal
+añadiendo el propio dominio target, filtrar por low, medium, high y critical, las info
+no tienen que salir)"""
